b9cfe38f748b0a808ab6063d93c39a92fa02b455
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2025-11-12 02:35:40 -0300

    agregamos boton con evento click a las tarjetas

--- js/index.js
+++ js/index.js
@@ -2,19 +2,22 @@ const arrayProductos = [
   {
     nombre: "Pelota LLamarada Moe",
     precio: 65000,
-    descripcion: "Con un tamaño de nº7 y una circunferencia de 74 cm",
+    descripcion:
+      "Con un tamaño de nº7 y una circunferencia de 74 cm, esta pelota está diseñada para ofrecer un excelente control y manejo en la cancha. Fabricada con material de caucho de alta calidad y una cámara de butilo, garantiza durabilidad y resistencia en cualquier tipo de superficie, ya sea interior o exterior.",
     nombreImagen: "./imagenes/pelota.jpg",
   },
   {
     nombre: "Zapatillas MG 20",
     precio: 170000,
-    descripcion: "Eleva tu rendimiento y domina cada rincón",
+    descripcion:
+      "Eleva tu rendimiento y domina cada rincón de la cancha con las nuevas [Nombre de las zapatillas]. Diseñadas para jugadores que exigen lo máximo, estas zapatillas combinan tecnología de punta con un estilo imponente. Su sistema de amortiguación de doble capa absorbe el impacto en cada salto y aterrizaje, protegiendo tus articulaciones y permitiendo que te muevas con una explosividad sin igual.",
     nombreImagen: "./imagenes/zapatillas.jpg",
   },
   {
     nombre: "Aro Exterior",
     precio: 300000,
-    descripcion: "¡Prepárate para encestar y sentir la gloria!",
+    descripcion:
+      "¡Prepárate para encestar y sentir la gloria con este aro de baloncesto profesional! Construido con materiales resistentes y un diseño aerodinámico, este aro te dará la precisión y potencia que necesitas para tus entrenamientos, garantizando horas de diversión y mejora constante de tu juego. ¡Conviértete en el rey de la cancha y anota puntos para la victoria!",
     nombreImagen: "./imagenes/juegos.png",
   },
 ];
@@ -47,9 +50,18 @@ arrayProductos.forEach((productos) => {
   //precio.classList.add("clase-precio");
   precio.textContent = `Precio: $${productos.precio}`;
 
+  //creo boton
+  let botonProducto = document.createElement("button");
+  botonProducto.classList.add("btn-productos");
+  botonProducto.textContent = "Ver Descripcion";
+  botonProducto.addEventListener("click", () => {
+    tarjeta.appendChild(descripcionProducto);
+  });
+
   tarjeta.appendChild(tituloProducto);
   tarjeta.appendChild(imagen);
-  tarjeta.appendChild(descripcionProducto);
+  // tarjeta.appendChild(descripcionProducto);
+  tarjeta.appendChild(botonProducto);
   tarjeta.appendChild(precio);
 
   divTarjetas.appendChild(tarjeta);
