b37f47c91f8a251619979d194a6d4ed64932341c
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2025-11-12 01:11:06 -0300

    Renderizado de tarjetas con js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,56 @@
 const arrayProductos = [
-  { nombre: "Producto 1", precio: 2000, descripcion: "Descripcion producto 1" },
-  { nombre: "Producto 2", precio: 4000, descripcion: "Descripcion producto 2" },
-  { nombre: "Producto 3", precio: 7000, descripcion: "Descripcion producto 3" },
+  {
+    nombre: "Pelota LLamarada Moe",
+    precio: 65000,
+    descripcion: "Con un tamaño de nº7 y una circunferencia de 74 cm",
+    nombreImagen: "./imagenes/pelota.jpg",
+  },
+  {
+    nombre: "Zapatillas MG 20",
+    precio: 170000,
+    descripcion: "Eleva tu rendimiento y domina cada rincón",
+    nombreImagen: "./imagenes/zapatillas.jpg",
+  },
+  {
+    nombre: "Aro Exterior",
+    precio: 300000,
+    descripcion: "¡Prepárate para encestar y sentir la gloria!",
+    nombreImagen: "./imagenes/juegos.png",
+  },
 ];
 
 const sectionContainer = document.getElementById("productos-home");
 let divTarjetas = document.createElement("div");
-divTarjetas.classList.add("clase-tarjetas");
+divTarjetas.classList.add("contenedor-tarjetas");
 
 arrayProductos.forEach((productos) => {
   //creo el article
   let tarjeta = document.createElement("article");
-  tarjeta.classList.add("clase-articulo");
+  tarjeta.classList.add("tarjeta-producto");
+
+  //creo la imagen
+  let imagen = document.createElement("img");
+  imagen.src = productos.nombreImagen;
 
   //titulo
-  let titulo = document.createElement("h2");
-  titulo.classList.add("clase-titulo");
-  titulo.textContent = productos.nombre;
+  let tituloProducto = document.createElement("h3");
+  // titulo.classList.add("clase-titulo");
+  tituloProducto.textContent = productos.nombre;
+
+  //descripcion
+  let descripcionProducto = document.createElement("p");
+  //descripcion.classList.add("clase-descripcion");
+  descripcionProducto.textContent = productos.descripcion;
 
   //precio
   let precio = document.createElement("p");
-  precio.classList.add("clase-precio");
+  //precio.classList.add("clase-precio");
   precio.textContent = `Precio: $${productos.precio}`;
 
-  //descripcion
-  let descripcion = document.createElement("p");
-  descripcion.classList.add("clase-descripcion");
-  descripcion.textContent = productos.descripcion;
-
-  tarjeta.appendChild(titulo);
+  tarjeta.appendChild(tituloProducto);
+  tarjeta.appendChild(imagen);
+  tarjeta.appendChild(descripcionProducto);
   tarjeta.appendChild(precio);
-  tarjeta.appendChild(descripcion);
 
   divTarjetas.appendChild(tarjeta);
 });
